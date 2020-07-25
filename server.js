dea0a5a3def9741f7c6d81d18f28ff26ed924881
vivek <narayanvivek71@gmail.com>
2020-07-25 14:31:50 +0530

    server commenting

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+//create server start
 const  app = require("./app");
 const port = process.env.PORT?process.env.PORT: 3000;
 app.listen(port, () => console.log(`magicmind ${port}!`));
+//create server end
 
-require("./middleware/serverMiddleWare");
+//env config start
 require('dotenv').config();
+//env config end
 
+
+
+//middleware start
+require("./middleware/serverMiddleWare");
+//middleware end
+
+
+//All other routes should be redirected to 404 start
 app.get('*', function(req, res){
     res.send('not found', 404);
   });
 
+//All other routes should be redirected to 404 end
